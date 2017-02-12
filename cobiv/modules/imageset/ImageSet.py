776d491cf8cb17cba62055c93cdb319c6d14671c
gokudomatic <gourry.gabrief@gmail.com>
2017-02-12 12:39:47 +0100

    converted image viewer from carousel to scrollview

--- cobiv/modules/imageset/ImageSet.py
+++ cobiv/modules/imageset/ImageSet.py
@@ -1,12 +1,75 @@
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.properties import NumericProperty, ObjectProperty
 from kivy.uix.image import AsyncImage
+from enum import Enum
 
+Builder.load_file('modules/imageset/slide.kv')
+
+class SlideMode(Enum):
+    NORMAL=0
+    FIT_SCREEN=1
+    FIT_WIDTH=2
+    FIT_HEIGHT=3
+
+
+class Slide(AsyncImage):
+
+    mode=ObjectProperty(None)
+
+    is_loaded=False
+
+    def __init__(self, **kwargs):
+        super(Slide, self).__init__(**kwargs)
+        self._coreimage.bind(on_load=self.on_image_loaded)
+        self.bind(width=self.on_width)
+        self.bind(height=self.on_height)
+        self.bind(mode=self.on_mode)
+        self.bind(texture_size=self.on_texture_size)
+
+    def on_texture_size(self, instance, value):
+        if self.is_loaded:
+            self.mode=SlideMode.FIT_HEIGHT
+
+
+    def on_image_loaded(self,*args):
+        self.is_loaded=True
+
+    def on_mode(self,obj,value):
+        print "mode="+str(value)
+        if value==SlideMode.FIT_HEIGHT:
+            self.size_hint=(None,1)
+        elif value==SlideMode.FIT_WIDTH:
+            self.size_hint=(1,None)
+        elif value==SlideMode.FIT_SCREEN:
+            self.size_hint=(1,1)
+        else:
+            self.size_hint=(None,None)
+            self.size=self.texture_size
+        self.size=self.texture_size
+        print "size_hint: "+str(self.size_hint)
+        print "texture_size"+str(self.texture_size)
+
+
+    def on_width(self,obj,value):
+        if self.mode==SlideMode.FIT_WIDTH:
+            self.height = self.width / self.image_ratio
+
+    def on_height(self,obj,value):
+        if self.mode==SlideMode.FIT_HEIGHT:
+            self.width = self.height * self.image_ratio
+
+
+    @property
+    def zoom(self):
+        return self.width/self.texture_size[0]
 
 class ImageSet:
     uris = []
 
     def images(self):
         for uri in self.uris:
-            yield AsyncImage(source=uri, allow_stretch=True)
+            yield Slide(source=uri)
 
 
 current_imageset = ImageSet()
--- cobiv/modules/viewer/Viewer.py
+++ cobiv/modules/viewer/Viewer.py
@@ -1,44 +1,47 @@
+from kivy.core.image import Image
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.lib.osc import bind
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.carousel import Carousel
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.scatterlayout import ScatterLayout
 from kivy.uix.scrollview import ScrollView
 
-from cobiv.modules.imageset.ImageSet import current_imageset
+from cobiv.modules.imageset.ImageSet import current_imageset, SlideMode
 from cobiv.modules.view import *
 
 Builder.load_file('modules/viewer/viewer.kv')
 
 
-class Viewer(View, Carousel):
+class Viewer(View, ScrollView):
     def __init__(self, **kwargs):
         super(Viewer, self).__init__(**kwargs)
-        self.anim_move_duration=0.1
 
         self.load_imageset()
 
-        self.set_action("next",self.load_next)
-        self.set_action("previous",self.load_previous)
-        self.set_action("first",self.load_first)
+        # self.set_action("next",self.load_next)
+        # self.set_action("previous",self.load_previous)
+        # self.set_action("first",self.load_first)
+        # self.set_action("fit",self.fit_screen)
+        #
+        # self.set_hotkey(275,"next")
+        # self.set_hotkey(108L,"next")
+        # self.set_hotkey(106L,"previous")
+        # self.set_hotkey(276,"previous")
+        # self.set_hotkey(103L,"first")
 
-        self.set_hotkey(275,"next")
-        self.set_hotkey(108L,"next")
-        self.set_hotkey(106L,"previous")
-        self.set_hotkey(276,"previous")
-        self.set_hotkey(103L,"first")
-
-    def load_imageset(self,imageset=current_imageset):
+    def load_imageset(self, imageset=current_imageset):
         self.clear_widgets()
-        for i in imageset.images():
-            scatter=ScatterLayout(do_rotation=False)
-            scatter.add_widget(i)
-            self.add_widget(scatter)
+        image = imageset.images().next()
+        self.add_widget(image)
 
-    def load_first(self):
-        self.load_slide(self.slides[0])
+    # def load_first(self):
+    #     self.load_slide(self.slides[0])
 
     @staticmethod
     def get_name(instance=None):
         return "viewer"
+
 
 available_views[Viewer.get_name()] = Viewer
